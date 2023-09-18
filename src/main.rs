5344e9f493275ab9758feb1fa971819899dec4a5
Douglas <doug.su.au@gmail.com>
2023-09-18 11:52:56 +1000

    [EG-05] command line options

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,23 @@ mod version;
 use grep_impl::grep;
 use version::*;
 
+use std::env;
+
 fn main() {
     version_manipulation().unwrap();
 
-    let matched_lines = match grep::grep_from_file("/etc/passwd", "root") {
+    let mut args = env::args();
+    let _prog = args.next().unwrap();
+    let Some(pattern) = args.next() else {
+        println!("OPTION ERROR: missing PATTERN option");
+        return;
+    };
+    let Some(file) = args.next() else {
+        println!("OPTION ERROR: missing FILE option");
+        return;
+    };
+
+    let matched_lines = match grep::grep_from_file(&file, &pattern) {
         Ok(lines) => lines,
         Err(e) => {
             println!("GREP ERROR: {}", e.msg);
