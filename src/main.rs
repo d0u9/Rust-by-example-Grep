9d55acb623b7c67f85eb8b66cf2e4895b9ace386
Douglas <doug.su.au@gmail.com>
2023-09-18 11:34:11 +1000

    [EG-01] versioning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+    version_manipulation();
+}
+
+fn version_manipulation() {
+    let is_dev = true;
+    let version = String::from("5.16");
+    let suffix = "-dev";
+
+    if version.find('.').is_none() {
+        println!("invalid version number");
+        return;
+    }
+
+    version_verify_and_compare(&version).unwrap();
+
+    print_version(version, suffix, is_dev);
+}
+
+fn print_version(mut version: String, suffix: &str, is_dev: bool) {
+    if is_dev {
+        version += suffix;
+    }
+    println!("version: {}", version);
+}
+
+fn version_verify_and_compare(version: &str) -> Result<(), std::num::ParseFloatError> {
+    let version_f64 = match version.parse::<f64>() {
+        Ok(v) => v,
+        Err(e) => return Err(e),
+    };
+
+    if version_f64 > 5.0 {
+        println!("version is greater than 5.0");
+    }
+    Ok(())
 }
