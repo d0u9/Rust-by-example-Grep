5594107f449693f9ba424c222b4a2d0cd4acc7ad
Douglas <doug.su.au@gmail.com>
2023-09-18 12:03:47 +1000

    [EG-08] generic

--- src/grep_impl/grep.rs
+++ src/grep_impl/grep.rs
@@ -2,11 +2,26 @@ use crate::err::AppErr;
 use crate::grep_impl::find_greper::*;
 use crate::grep_impl::interface::*;
 use crate::grep_impl::regex_greper::*;
+use std::env;
 
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 pub fn grep_from_file(file: &str, needle: &str) -> Result<Vec<String>, AppErr> {
+    match env::var("BACKEND") {
+        Ok(backend) => match backend.to_ascii_lowercase().as_ref() {
+            "regex" => grep_from_file_by_greper(file, RegexGreper::new(needle)?),
+            _ => grep_from_file_by_greper(file, FindGreper::new(needle)?),
+        },
+        Err(_) => grep_from_file_by_greper(file, RegexGreper::new(needle)?),
+    }
+}
+
+fn grep_from_file_by_greper<G>(file: &str, greper: G) -> Result<Vec<String>, AppErr>
+where
+    G: Greper,
+{
+    println!("==>> Using {}", greper.name());
     // Read file, and return error if `open()` failed
     let file = match File::open(file) {
         Ok(file) => file,
@@ -14,12 +29,6 @@ pub fn grep_from_file(file: &str, needle: &str) -> Result<Vec<String>, AppErr> {
     };
 
     let mut matched_lines = Vec::new();
-
-    // Use either one you prefer
-    let greper = RegexGreper::new(needle)?;
-    let greper = FindGreper::new(needle)?;
-
-    println!("===>>> Using {}", greper.name());
 
     // Create a BufReader for our file
     let reader = BufReader::new(file);
